72d2c2167718887cc67412b0bdeda2b7f20faa9e
Thomas Chevallier <81810759+258498@users.noreply.github.com>
2026-03-30 17:04:14 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js — Remplace les valeurs par celles de ta console Firebase
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSy-REMPLACE",
-  authDomain: "mon-projet.firebaseapp.com",
-  projectId: "mon-projet",
-  storageBucket: "mon-projet.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abc123"
-};
